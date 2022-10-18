8760f3ab2af8c1d0a1fdafe06ebc352acf10f097
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-10-18 16:59:31 +0300

    jwt-cookie-parser fronted

---
+++ app.vue
@@ -0,0 +1,59 @@
+<script>
+export default {};
+</script>
+<template>
+  <main class="main">
+    <nav class="navs">
+      <NuxtLink to="/"> <h3>Home</h3></NuxtLink>
+      <div class="auth">
+        <NuxtLink to="register">Register</NuxtLink>
+        <NuxtLink to="login">login</NuxtLink>
+      </div>
+    </nav>
+    <NuxtPage />
+  </main>
+</template>
+
+<style>
+body {
+  background: linear-gradient(90deg, teal, black, indigo);
+  color: wheat;
+}
+main {
+  position: relative;
+}
+.navs {
+  margin-bottom: 50px;
+  justify-items: end;
+  position: fixed;
+  background-color: rgb(95, 42, 38);
+  padding: 9px;
+  z-index: 2;
+  width: 100%;
+  padding-bottom: 20px;
+  align-items: flex-end;
+  display: flex;
+  color: white;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  margin: auto;
+}
+.auth {
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+  display: flex;
+  gap: 9px;
+  color: white;
+  margin: auto;
+  text-decoration: none;
+}
+a {
+  color: aliceblue;
+  text-decoration: none;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+a:hover {
+  border-radius: 3px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main-home">
+    <h4 class="h">You are not logged in</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+body {
+  background-color: blue;
+}
+.main-home {
+  margin-top: 20px;
+  display: flex;
+  height: 200px;
+  background-color: teal;
+  border-radius: 7px;
+  box-sizing: border-box;
+  box-shadow: black;
+  justify-items: center;
+  align-items: center;
+  padding: 30px;
+  width: 50%;
+  color: black;
+  font-family: "Courier New", Courier, monospace;
+  font-size: x-large;
+  margin: auto;
+}
+.h {
+  justify-items: center;
+  align-items: center;
+  text-align: center;
+  display: flex;
+  color: white;
+  margin: auto;
+}
+</style>
